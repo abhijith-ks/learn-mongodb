95a66c04be95f9cdfe3526572674e9c0d0d01cde
abhijith-ks <nandu.abhi2@gmail.com>
2022-01-01 12:29:14 +0530

    Added collection name for the model

--- src/main/java/io/abhijith/basicspringbootskeleton/model/BasicModel.java
+++ src/main/java/io/abhijith/basicspringbootskeleton/model/BasicModel.java
@@ -1,8 +1,10 @@
 package io.abhijith.basicspringbootskeleton.model;
 
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document("basic-collection")
 public class BasicModel {
 
     String id;
